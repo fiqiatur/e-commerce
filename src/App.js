fe4c4e3a6eac842125b718bd2def38b971da40b0
fiqiatur <fiqiatur.hrp@gmail.com>
2020-06-01 19:19:24 +0700

    add header & styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Route, Switch,  } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
-import ShopPage from '../src/pages/shop/shop.component'
-import './App.css'
-
+import ShopPage from "../src/pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import "./App.css";
 
 function App() {
    return (
       <div>
+         <Header />
          <Switch>
             <Route exact path="/" component={Homepage} />
             <Route path="/shop" component={ShopPage} />
