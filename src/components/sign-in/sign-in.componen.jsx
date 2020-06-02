fadda43a8c265744bf773974a77565aa25457fbe
fiqiatur <fiqiatur.hrp@gmail.com>
2020-06-02 09:18:53 +0700

    continue signin form & styling

--- src/components/sign-in/sign-in.componen.jsx
+++ src/components/sign-in/sign-in.componen.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./sign-in.componen.style.scss";
+import FormInput from "../form-input/form-input.componen";
 
 export class SignIn extends Component {
    constructor(props) {
@@ -28,22 +29,22 @@ export class SignIn extends Component {
             <h2>I alredy</h2>
             <span>Signin wit email & password</span>
 
-            <form onSubmit={this.handleSubmit}>
-               <label>Email</label>
-               <input
+            <form onSubmit={this.handleSubmit} autocomplete="off">
+               <FormInput
                   type="email"
                   name="email"
                   value={this.state.email}
-                  onChange={this.handleChange}
+                  handleChange={this.handleChange}
+                  label="email"
                   required
                />
-               <label>Password</label>
 
-               <input
+               <FormInput
                   type="password"
                   name="password"
                   value={this.state.password}
-                  onChange={this.handleChange}
+                  handleChange={this.handleChange}
+                  label="password"
                   required
                />
 
