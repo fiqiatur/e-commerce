379c1f2f6752ee004480e2b2493a70d50ea886aa
fiqiatur <fiqiatur.hrp@gmail.com>
2020-06-03 10:51:43 +0700

    fix typo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "../src/pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import Sign from "./pages/singn-singup/signin-up";
-import "./App.css";
 import { auth } from "./firebase/firebase.utils";
+import "./App.css";
 
 class App extends Component {
    constructor() {
@@ -15,7 +15,7 @@ class App extends Component {
          currentUser: null,
       };
    }
-   
+
    unsubscribeFromAuth = null;
 
    componentDidMount() {
@@ -26,14 +26,14 @@ class App extends Component {
       });
    }
 
-   componentWillMount() {
+   componentWillUnmount() {
       this.unsubscribeFromAuth();
    }
 
    render() {
       return (
          <div>
-            <Header />
+            <Header currentUser={this.state.currentUser} />
             <Switch>
                <Route exact path="/" component={Homepage} />
                <Route path="/shop" component={ShopPage} />
