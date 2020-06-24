e3a10d80ae8e49b59d06950cb5d842a20dac08bf
fiqiatur <fiqiatur.hrp@gmail.com>
2020-06-24 19:47:02 +0700

    c

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
+import Footer from '../../components/Footer/footer'
 
 export default function homepagecomponent() {
    return (
@@ -8,6 +9,7 @@ export default function homepagecomponent() {
          <div className="homepage">
            <Directory />
          </div>
+           <Footer />
       </div>
    );
 }
