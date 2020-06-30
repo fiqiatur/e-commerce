43a3a55aa7647ca6501307774508b10b5297abd0
fiqiatur <fiqiatur.hrp@gmail.com>
2020-06-30 18:29:46 +0700

    finishing

--- src/components/sign-in/sign-in.componen.jsx
+++ src/components/sign-in/sign-in.componen.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./sign-in.componen.style.scss";
 import FormInput from "../form-input/form-input.componen";
 import CustomButton from "../custom-button/custom-button-componen";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 export class SignIn extends Component {
    constructor(props) {
@@ -14,10 +14,17 @@ export class SignIn extends Component {
       };
    }
 
-   handleSubmit = (e) => {
+   handleSubmit = async (e) => {
       e.preventDefault();
 
-      this.setState({ email: "", password: "" });
+      const { email, password } = this.state;
+
+      try {
+         await auth.signInWithEmailAndPassword(email, password);
+         this.setState({ email: "", password: "" });
+      } catch (error) {
+         console.log(error);
+      }
    };
 
    handleChange = (e) => {
